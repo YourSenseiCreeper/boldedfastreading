49618860ff7599b4a711d1a56f326a73a541eb2d
Piotr Wereszczyński <tomanycreeps@gmail.com>
2022-06-01 18:21:39 +0200

    Accent color input

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ function inputChanged() {
     let value = document.getElementById('input').value;
     let fontSize = document.getElementById('fontSize').value;
     let fontColor = document.getElementById('fontColor').value;
+    let accentColor = document.getElementById('accentColor').value;
+    
+    value = value.replaceAll(/గ/g, 'j').replaceAll(/\n/g, ' ').replaceAll(/ǳ/g, 'dz');
+
     let words = value.split(' ');
     result = "";
     words.forEach(element => {
         let splitIndex = element.length / 2;
-        let outputWord = `<span class="bold">${element.slice(0, splitIndex)}</span>${element.slice(splitIndex, element.length)}`;
+        let outputWord = `<span class="bold" style="color: ${accentColor}">${element.slice(0, splitIndex)}</span>${element.slice(splitIndex, element.length)}`;
         result += outputWord + ' ';
     });
     document.getElementById('output').innerHTML = result;
