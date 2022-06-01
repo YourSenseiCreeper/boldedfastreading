eacd6de820d99ea91eba6b9c39e4509c31e69203
Piotr Wereszczyński <tomanycreeps@gmail.com>
2022-06-01 17:55:36 +0200

    Style changes

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function inputChanged() {
     let value = document.getElementById('input').value;
-    let fontSizeControl = docum
+    let fontSize = document.getElementById('fontSize').value;
+    let fontColor = document.getElementById('fontColor').value;
     let words = value.split(' ');
-    result = '';
+    result = "";
     words.forEach(element => {
         let splitIndex = element.length / 2;
         let outputWord = `<span class="bold">${element.slice(0, splitIndex)}</span>${element.slice(splitIndex, element.length)}`;
         result += outputWord + ' ';
     });
     document.getElementById('output').innerHTML = result;
+    document.getElementById('output').style.fontSize = fontSize + 'pt';
+    document.getElementById('output').style.color = fontColor;
 }
